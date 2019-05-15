c9eba1f6b2acd65c1244764dcbe641a5b3c017e1
Atzuke <c.riman@hotmail.com>
2019-05-15 15:51:36 -0500

    Ya funcionan el mapping
    Se arreglo el mapping, ahora se puede ir de una pagina a otra, ambas estan en templates no queda nada en static. (resources)

--- FacturaNet_Web/src/main/java/co/edu/uco/facturanet/web/factura/FacturaNetcontroller.java
+++ FacturaNet_Web/src/main/java/co/edu/uco/facturanet/web/factura/FacturaNetcontroller.java
@@ -1,17 +1,23 @@
-package co.edu.uco.facturanet.controller;
+package co.edu.uco.facturanet.web.factura;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class FacturaNetcontroller {
 
-	@GetMapping("/factura")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") 
-    String name, Model model) {
+	
+	@GetMapping ("/inicio")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping ("/factura")
+    public String factura(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "factura";
 	}
